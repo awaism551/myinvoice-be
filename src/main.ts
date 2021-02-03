4abea81b7b8b392f161069f79b10ad63bc2200a9
Awais <awaism551@gmail.com>
2021-02-03 15:05:24 +0500

    heroku test

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   var server_port = process.env.PORT || 3000;
-  var server_host = 'localhost' || '0.0.0.0';
+  var server_host = '0.0.0.0';
   await app.listen(server_port, server_host);
 }
 bootstrap();
